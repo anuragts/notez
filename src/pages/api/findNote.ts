95b6b885576591a781eb1cf9fe09f8834621a80e
K-Shivam009 <114254652+K-Shivam009@users.noreply.github.com>
2022-10-23 15:24:14 +0530

    Update findNote.ts

--- src/pages/api/findNote.ts
+++ src/pages/api/findNote.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../db/client";
 // Currently using id as the unique identifier for a note , will use user id later
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.body;
+    const { id } =  req.body;
     const note = await prisma.notes.findUnique({
         where: {
             id : Number(id)
